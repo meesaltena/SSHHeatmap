3af5f597a39512a169070664467e21f17d6d0115
meesaltena <Mees.altena@gmail.com>
2023-12-15 17:55:38 +0100

    use argparse instead of argv, remove unused imports, update readme

--- SSHHeatmap.py
+++ SSHHeatmap.py
@@ -2,82 +2,71 @@
 # Licence: MIT 
 import re
 import folium
-from folium.plugins import HeatMap
 import ipinfo
-import sys
-import time
+import argparse
+from sys import exit
+from folium.plugins import HeatMap
+from time import process_time
 from collections import Counter
 
-# ipinfo api key here if you're not using sys arguments.
-api_key = ""
+# Set ipinfo api key here if you're not using arguments.
+key = ""
 
-# Filename of the txt with the output of: grep "authentication failure\| Failed password" /var/log/auth.log > failed_attempts.txt
-try:
-    filename = sys.argv[1]
-except IndexError:
-    if(api_key == ""):
-        print("Usage: SSHHeatmap.py <source_filename> <api key> <attempts_threshold> <heatmap_filename>")
-        print("To run SSHHeatmap without arguments, manually set an api key.")
-        quit()
-    filename = "failed_attempts.txt"
-    pass
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-i", "--input",
+                    default="failed_attempts.txt",
+                    help="Input filepath of: grep \"authentication failure\| Failed password\" /var/log/auth.log > [filename]")
+parser.add_argument("-t", "--threshold", 
+                    default=50,
+                    type=int,
+                    help="Minimum number of attempts before an ip is included in the heatmap")
+parser.add_argument("-o", "--output",
+                    default="heatmap.html",
+                    help="Filename of the heatmap output")
+required = parser.add_argument_group('required arguments')
+required.add_argument("-k", "--key",
+                    default = key,
+                    required = (key == ""),
+                    metavar = "API_KEY",
+                    help="ipinfo.io api key")
+args = parser.parse_args()
 
-try:
-    api_key = sys.argv[2]
-except IndexError:
-    if(api_key == ""):
-        raise IndexError("API key not found. Please pass your ipinfo.io api key as the second argument, or set it manually.")
+ip_handler = ipinfo.getHandler(args.key)
 
-try:
-    min_attempts = int(sys.argv[3])
-except IndexError:
-    min_attempts = 30
-    pass    
-
-try:
-    heatmap_filename = sys.argv[4]
-except IndexError:
-    heatmap_filename = 'heatmap.html'
-    pass
-
-ip_handler = ipinfo.getHandler(api_key)
-
-def read_file_get_ips(filename):
+def read_file_get_ips():
     pattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-
-    with open(filename) as f:
+    with open(args.input) as f:
         file = f.read()
         ips = pattern.findall(file)
-
-        print(f"Read file {filename} and got {len(ips)} login attempts.")
+        print(f"Read file {args.input} and got {len(ips)} login attempts.")
         return ips
     
 def get_applicable_ips(ips):
     counts = Counter(ips).most_common()
-    meet_minimum = [x[0] for x in counts if x[1] > min_attempts]
-    print(f"No. of ips with at least {min_attempts} login attempts: {len(meet_minimum)}")
+    meet_minimum = [x[0] for x in counts if x[1] > args.threshold]
+    print(f"No. of ips with at least {args.threshold} login attempts: {len(meet_minimum)}")
     return meet_minimum
 
 def get_ip_coordinates(ips):
     try:
-        start = time.process_time()
-        batchDetails = ip_handler.getBatchDetails(ips, batch_size=100).values()
-        coords =[x['loc'].split(',') for x in batchDetails if 'loc' in x.keys()]
-        print(f"Fetched {len(coords)}/{len(ips)} coordinates in {round(time.process_time() - start, 3)} seconds.")
+        start = process_time()
+        batchresult = ip_handler.getBatchDetails(ips, batch_size=100).values()            
+        coords =[x['loc'].split(',') for x in batchresult if 'loc' in x.keys()]
+        print(f"Fetched {len(coords)}/{len(ips)} coordinates in {round(process_time() - start, 3)} seconds.")
+        return coords
     except Exception as ex:
         print(ex)
-         
-    return coords       
-
+        exit(1)
+       
 def generate_and_save_heatmap(coords):        
     m = folium.Map(tiles="OpenStreetMap", location=[20,10], zoom_start=2)
     # mess around with these values to change how the heatmap looks
     HeatMap(data = coords, radius=15, blur=20, max_zoom=2).add_to(m)
-    m.save(heatmap_filename) 
-    print(f"Done. heatmap saved as {heatmap_filename}")  
+    m.save(args.output) 
+    print(f"Done. heatmap saved as {args.output}")  
 
 def main():    
-    ips = read_file_get_ips(filename)
+    ips = read_file_get_ips()
     ips_count = get_applicable_ips(ips)
     coords = get_ip_coordinates(ips_count)
     generate_and_save_heatmap(coords)
